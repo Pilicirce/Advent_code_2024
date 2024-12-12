978d1b7fb12be510354e08bd5dd4d4eaf2bc1c2d
Pilar Solis <pilar.sm02@gmail.com>
2024-12-12 07:55:10 +0100

    "try" to solve Day2_Part1 (but there are some errors still)

--- src/main/java/com/adventcode2024/day2/Day2_Part1.java
+++ src/main/java/com/adventcode2024/day2/Day2_Part1.java
@@ -18,7 +18,7 @@ public class Day2_Part1 {
 
         System.out.println("SOLVING EXERCISE DAY 2, PART 1 : ");
 
-        String filePath = "C:\\Users\\msolisma\\Desktop\\PILI\\CURSOS\\AdventCode\\Advent_code_2024\\inputDia2_2024.txt";
+        String filePath = "C:\\Users\\msolisma\\Desktop\\PILI\\CURSOS\\AdventCode\\Advent_code_2024\\inputDia2_2024_ejemplo.txt";
 
         try {
         //1) Open the file for reading
@@ -32,12 +32,33 @@ public class Day2_Part1 {
         //3) Iterate over each line of the file and split the line into parts (levels separados por espacios)
                 
             while ((report = reader.readLine()) != null) {
-                // Divide la línea en diferentes números (leves) usando split con un espacio y lo almacena en un array
+                // Divide la línea en diferentes números (levels) usando split con un espacio y lo almacena en un array
                 String[] parts = report.trim().split(" ");
+               
                 for (String part : parts) {
                     levelsReport.add(Integer.parseInt(part));
-                }             
-            }
+                }
+                
+            //     // Comparar los levels de cada report: es safe si se cumplen estas dos condiciones: 
+            //     // -The levels are either all increasing or all decreasing.
+            //     // -Any two adjacent levels differ by at least one and at most three.
+            //     boolean isSafe = true;
+            //     for (int i = 0; i < levelsReport.size() - 1; i++) {
+            //         int diff = levelsReport.get(i) - levelsReport.get(i + 1);
+            //         if (diff >= 1 || diff <= 3) {
+            //             isSafe = true;
+            //             totalSafeReports++;
+            //             break;
+            //         }
+            //     }
+                
+            //     if (isSafe) {
+            //         System.out.println("El report " + levelsReport + " es safe");
+            //         totalSafeReports++;
+            //     } else {
+            //         System.out.println("El report " + levelsReport+ " NO es safe");
+            //     }
+            // }
 
         //4) Comparar los levels de cada report: es safe si se cumplen estas dos condiciones: 
         //-The levels are either all increasing or all decreasing.
@@ -46,9 +67,12 @@ public class Day2_Part1 {
             if (levelsReport.get(i) - levelsReport.get(i + 1) >= 1 && levelsReport.get(i) - levelsReport.get(i + 1) <= 3) {
 
                 //5) Contar los reports que son safe (y añadirla a una variable sumatoria)
-                totalSafeReports++;
+                // totalSafeReports++;
+                System.out.println("El report " + levelsReport + "es safe : " + totalSafeReports);
+                totalSafeReports = totalSafeReports + 1;
 
             } else {
+                System.out.println("El report " + levelsReport + " NO es safe");
             }
         }
 
@@ -58,9 +82,10 @@ public class Day2_Part1 {
         //7) Show the result
         System.out.println("Número total de reports que son safe: " + totalSafeReports);
 
+    }
     } catch (IOException e) {
          e.printStackTrace();
-         }
+    }
 
     }
 }
